ef6652e06ec372c433aefb2ee656aef721ae4e17
KGB <kevin@kevinblackman.com>
2010-08-22 23:09:08 +0200

    Finished first cut of the User model

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+# Schema version: 20100813161503
+#
+# Table name: deals
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  content     :string(255)
+#  exp_date    :date
+#  showclaimed :boolean
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Deal < ActiveRecord::Base
 	belongs_to :user
 	validates_presence_of :name
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
-class User < ActiveRecord::Base  
-	has_many :deals
-	validates_presence_of :name
-	validates_length_of :email, :minimum => 5
- end
+# == Schema Information
+# Schema version: 20100813161503
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+	attr_accessible :name, :email
+	
+	EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
+	validates_presence_of :name, :email
+	validates_length_of   :name, :maximum => 50
+	validates_format_of :email, :with => EmailRegex
+	validates_uniqueness_of :email, :case_sensitive => false
+
+end
